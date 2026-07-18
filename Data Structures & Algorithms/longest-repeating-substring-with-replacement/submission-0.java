61fcde11b6c8e1a9379e2a9ec2990e908d1ad0a8
luiskywalker <luiskywalker@gmail.com>
2026-07-17 22:33:31 -0600

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int [] charFrequency = new int[26];//array to store freq count of each uppercase (A-Z)
+
+        int left = 0;//left pointer
+
+        int maxFrequency = 0;
+
+        int length = s.length();
+
+        for(int right = 0; right < length; right++){
+            int currentCharIndex =  s.charAt(right) - 'A';
+            charFrequency[currentCharIndex]++;
+            maxFrequency = Math.max(maxFrequency, charFrequency[currentCharIndex]);
+            int windowSize = right - left +1;
+            int charactersToReplace = windowSize - maxFrequency;
+
+            if(charactersToReplace > k){
+                int leftCharIndex = s.charAt(left) - 'A';
+                charFrequency[leftCharIndex]--;
+                left++;
+            }
+        }
+        return length-left;
+    }
+}
